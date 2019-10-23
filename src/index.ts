8925ea9b9294fc725b48acdc7eed9e7ff3737d6d
qiyu1987 <qiyu1987@gmail.com>
2019-10-23 21:36:51 +0200

    setup DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
 import PageController from "./pages/controller"
+import setupDb from './db'
 
 const port = process.env.PORT || 4000
 
@@ -8,4 +9,8 @@ const app = createKoaServer({
    controllers: [PageController]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+  )
+  .catch(err => console.error(err))
