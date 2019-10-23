297d22439b6f8c8b042dc613b020f6573a740885
qiyu1987 <qiyu1987@gmail.com>
2019-10-23 17:27:30 +0200

    add decoration comments

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,12 +1,17 @@
 // src/pages/controller.ts
 import { JsonController, Get, Param } from 'routing-controllers'
 import pagesById, { Page } from './data'
-
+// this makes sure a class is marked as controller that always returns JSON
+// perfect for our REST API
 @JsonController()
+// this markes a method as endpoint
+// in this case it responds to any GET /pages/:id
+// request with :id being a variable parameter
 export default class PageController {
 
     @Get('/pages/:id')
     getPage(
+        // this decorator retrieves the ID parameter from the url
         @Param('id') id: number
     ): Page {
         return pagesById[id]
