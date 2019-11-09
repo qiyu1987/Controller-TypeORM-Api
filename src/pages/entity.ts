d1d438d494de071c25283ab36bc692a0aa18fc6b
qiyu1987 <qiyu1987@gmail.com>
2019-11-09 16:22:20 +0100

    add page model and create table in pg

---
+++ src/pages/entity.ts
@@ -0,0 +1,15 @@
+// src/pages/entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { BaseEntity } from "typeorm/repository/BaseEntity"
+
+@Entity()
+export default class Page extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id?: number
+
+	@Column("text", { nullable: false })
+	title: string
+
+	@Column("text", { nullable: false })
+	content: string
+}
