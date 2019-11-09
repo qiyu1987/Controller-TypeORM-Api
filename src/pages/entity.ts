5c0eb11d435fd138f8470d2d790052ec618e7b55
qiyu1987 <qiyu1987@gmail.com>
2019-11-09 17:08:48 +0100

    class validator

--- src/pages/entity.ts
+++ src/pages/entity.ts
@@ -1,15 +1,20 @@
 // src/pages/entity.ts
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 import { BaseEntity } from "typeorm/repository/BaseEntity"
+import { IsString, Length, MinLength } from "class-validator"
 
 @Entity()
 export default class Page extends BaseEntity {
 	@PrimaryGeneratedColumn()
 	id?: number
 
-	@Column("text", { nullable: false })
+	@IsString()
+	@Length(5, 25)
+	@Column("text")
 	title: string
 
-	@Column("text", { nullable: false })
+	@IsString()
+	@MinLength(10)
+	@Column("text")
 	content: string
 }
