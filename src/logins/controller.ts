68732184390c08a847d166a1bd74c1736fa7a2a9
qiyu1987 <qiyu1987@gmail.com>
2019-11-09 17:52:48 +0100

    jwt and login

--- src/logins/controller.ts
+++ src/logins/controller.ts
@@ -7,6 +7,7 @@ import {
 	BadRequestError
 } from "routing-controllers"
 import User from "../users/entity"
+import { sign } from "../jwt"
 
 // src/logins/controller.ts
 class AuthenticatePayload {
@@ -22,10 +23,12 @@ export default class LoginController {
 	@Post("/logins")
 	async authenticate(@Body() { email, password }: AuthenticatePayload) {
 		const user = await User.findOne({ where: { email } })
-		if (!user)
+		if (!user || !user.id)
 			throw new BadRequestError("A user with this email does not exist")
 		if (!(await user.checkPassword(password)))
 			throw new BadRequestError("The password is not correct")
+		const jwt = sign({ id: user.id })
+		return { jwt }
 		// if password is correct
 		// send back a jwt token
 		// else: send some HTTP 400 Bad request error
