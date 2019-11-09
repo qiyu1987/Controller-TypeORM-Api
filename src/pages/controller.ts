a416cadbb0fa3e8ba9e5a50206d5bfab0c87dfad
qiyu1987 <qiyu1987@gmail.com>
2019-11-09 16:39:18 +0100

    useing typeorm to read from database

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,45 +1,47 @@
 // src/pages/controller.ts
-import { JsonController, Get, Param, Put, Body, Post, HttpCode } from 'routing-controllers'
-import pagesById, { Page } from './data'
-type PageList = { pages: Page[] }
+import {
+	JsonController,
+	Get,
+	Param,
+	Put,
+	Body,
+	Post,
+	HttpCode
+} from "routing-controllers"
+import Page from "./entity"
 // this makes sure a class is marked as controller that always returns JSON
 // perfect for our REST API
 @JsonController()
 export default class PageController {
-    // this markes a method as endpoint
-    // in this case it responds to any GET /pages/:id
-    // request with :id being a variable parameter
-    
-    @Get('/pages/:id')
-    getPage(
-        // this decorator retrieves the ID parameter from the url
-        @Param('id') id: number
-        ): Page {
-            return pagesById[id]
-        }
-        
-        @Get('/pages')
-        allPages(): PageList {
-            // .. implement!
-            return {pages: Object.values(pagesById)}
-        }
-        
-        @Put('/pages/:id')
-        updatePage(
-            @Param('id') id: number,
-            @Body() body: Partial<Page>
-            ): Page {
-                console.log(`Incoming PUT body param:`, body)
-                return pagesById[id]
-            }
-            
-            @Post('/pages')
-            @HttpCode(201)
-            createPage(
-                @Body() body: Page
-                ): Page {
-                    console.log(`Incoming POST body param:`, body)
-                    return body
-                }
-        }
-        
+	// this markes a method as endpoint
+	// in this case it responds to any GET /pages/:id
+	// request with :id being a variable parameter
+
+	@Get("/pages/:id")
+	getPage(
+		// this decorator retrieves the ID parameter from the url
+		@Param("id") id: number
+	) {
+		return Page.findOne(id)
+	}
+
+	@Get("/pages")
+	async allPages() {
+		const pages = await Page.find()
+		// .. implement!
+		return { pages: pages }
+	}
+
+	@Put("/pages/:id")
+	updatePage(@Param("id") id: number, @Body() body: Partial<Page>) {
+		console.log(`Incoming PUT body param:`, body)
+		return Page.findByIds([id])
+	}
+
+	@Post("/pages")
+	@HttpCode(201)
+	createPage(@Body() body: Page): Page {
+		console.log(`Incoming POST body param:`, body)
+		return body
+	}
+}
